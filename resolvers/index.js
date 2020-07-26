6d521104c61fe9bd0523a48373de56ec5a352992
IqraL <IqraLatif159@gmail.com>
2020-07-26 17:18:37 +0100

    getLibraries resolver & type

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require("sequelize");
 const cctvRawData = require("../data/CctvData.json");
+const librariesRawData = require("../data/librariesData.json");
 
 const sequelize = new Sequelize(
   process.env.DATABASE,
@@ -13,7 +14,24 @@ const sequelize = new Sequelize(
 
 module.exports = {
   Query: {
-    getCctvLocations: async () => {
+    getLibraries: () => {
+      const librariesProcessed = librariesRawData.features.map((library) => {
+        const revesedPoints = [];
+        revesedPoints.push(library.geometry.coordinates[1]);
+        revesedPoints.push(library.geometry.coordinates[0]);
+
+        return {
+          points: revesedPoints,
+          name: library.properties.NAME,
+          address: library.properties.ADDRESS_1,
+          postcode: library.properties.POSTCODE,
+          telepone: library.properties.TELEPHONE,
+        };
+      });
+
+      return librariesProcessed;
+    },
+    getCctvLocations: () => {
       const cctvProcessed = cctvRawData.features.map((cctvObj) => {
         const revesedPoints = [];
         revesedPoints.push(cctvObj.geometry.coordinates[1]);
--- schema/index.js
+++ schema/index.js
@@ -4,16 +4,20 @@ const GraphQLJSON = require("graphql-type-json");
 module.exports = gql`
   scalar JSON
 
-  type polygon {
-    markers: JSON
-  }
-
   type cctv {
     location: String
+    points: [Float]
+  }
+  type Library {
+    name: String
+    address: String
+    postcode: String
+    telepone: String
     points: [Float]
   }
   type Query {
     getConservationAreas: JSON
     getCctvLocations: [cctv]
+    getLibraries: [Library]
   }
 `;
