a92911a8c735adfc29ee41ea980f77f4ff5090f9
IqraL <IqraLatif159@gmail.com>
2020-08-01 14:10:34 +0100

    fixed heroku deployment error

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ const resolvers = require("./resolvers");
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+app.get("/", (req, res) => res.send("redirect your endpoint to /graphql"));
+
 app.use(cors());
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   playground: true,
-
+  introspection: true,
   context: ({ req, res }) => ({}),
 });
 
