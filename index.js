72296e8cb3f7ea187589514a79e33b2b4e1b6fc6
IqraL <IqraLatif159@gmail.com>
2020-08-01 14:14:47 +0100

    update

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ const app = express();
 app.get("/", (req, res) => res.send("redirect your endpoint to /graphql"));
 
 app.use(cors());
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  playground: true,
-  introspection: true,
-  context: ({ req, res }) => ({}),
-});
-
-server.applyMiddleware({ app });
+//
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   playground: true,
+//   introspection: true,
+//   context: ({ req, res }) => ({}),
+// });
+//
+// server.applyMiddleware({ app });
 
 app.listen({ port: PORT }, () =>
   console.log(` Server ready at http://localhost:4000${server.graphqlPath}`)
